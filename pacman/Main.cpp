4fa8860dd36f2c3589746469ea3e8ea18191c225
Itamar Podolsky <itamarpo@mta.ac.il>
2021-12-02 20:42:42 +0200

    updated notes

--- pacman/Main.cpp
+++ pacman/Main.cpp
@@ -10,3 +10,10 @@ int main()
 
 
 
+/*change enum to enum class in ghost
+GameObject: Fruit, Ghost, Pacman horasha yeshira. position muhal ba gameobject - beyahad
+Fruit class. - itamar
+fix code. - beyahad
+need to create 3 boards text. 3 levels, and create a function for them. - itay.
+smarter ghosts- beyahad.
+*/
